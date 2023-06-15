90de9982375517f19c4b2bdf0721a0ab84affeca
Tsur Hazan <111530146+TsurHazan@users.noreply.github.com>
2023-06-15 14:39:28 +0300

    tsurWay

--- UniqueIdsScannerUI/App.cs
+++ UniqueIdsScannerUI/App.cs
@@ -11,6 +11,7 @@ using UniqueIdsScannerUI;
 using Model;
 using Entity;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
 
 public class App
 {
@@ -33,7 +34,7 @@ public class App
         //args[2] = @"C:\ZionNet\DevOps\KLA\DataDictionaryValidation-ExampleApp\InputFiles\ATLAS.reassign.xml"; 
 
         args = new string[1];
-        args[0] = "--update"; 
+        args[0] = ""; 
 
         //if user acces without args params then start user interface
         if (args.Length == 0)
@@ -49,9 +50,6 @@ public class App
         }
         else
         {
-
-            var listOfXmlFileFromAppSettings = _settings.GetSection("XmlFilesPath").Get<List<string>>();
-
             using (var parser = new CommandLine.Parser((settings) =>
             {
                 settings.CaseSensitive = true;
@@ -60,18 +58,22 @@ public class App
                 //add logs
                 var result = Parser.Default.ParseArguments<CliOptions>(args)
                .WithParsed(Options => {
-                   if (string.IsNullOrWhiteSpace(Options.filePath))
-                   {
-                       Console.WriteLine("Invalid command line arguments");
-                       throw new ArgumentException("filePath is null or empty or white-space");
-                   }
-                   else
-                   {
 
+                   // user wanted 1 file option (-f path)
+                   if (!string.IsNullOrWhiteSpace(Options.filePath))
+                   {
                        RunVerifyAndUpdate(Options.filePath, Options.isUpdate);
                    }
+                   else //default config mode
+                   {
+                       List<string>? listOfXmlFileFromAppSettings = _settings.GetSection("XmlFilesPath").Get<List<string>>();
+                       if (listOfXmlFileFromAppSettings != null && _mainManager.ValidateXmlFilePaths(listOfXmlFileFromAppSettings))
+                       {
+                           listOfXmlFileFromAppSettings.ForEach(path => RunVerifyAndUpdate(path, Options.isUpdate));
+                       }
+                   }
                })
-               //if user enterd wrong arguments
+               //if user enterd unkown option/argument
                .WithNotParsed((errs) => throw new ArgumentException($"Failed to parse command line arguments: {errs}"));
             }
         }
@@ -107,50 +109,4 @@ public class App
 }
 
 
-//if (_mainManager.ValidateXmlFilePaths(listOfXmlFileFromAppSettings))
-//{
-//    using (var parser = new CommandLine.Parser((settings) =>
-//    {
-//        settings.CaseSensitive = true;
-//    }))
-//    {
-//        //add logs
-//        var result = Parser.Default.ParseArguments<CliOptions>(args)
-//       .WithParsed(Options => {
-//           if (string.IsNullOrWhiteSpace(Options.filePath))
-//           {
-//               Console.WriteLine("Invalid command line arguments");
-//               throw new ArgumentException("filePath is null or empty or white-space");
-//           }
-//           else
-//           {
-//               M_SeperatedScopes? xmlScopes = _mainManager.XmlToSeperatedScopes(Options.filePath);
-//               M_SeperatedScopes? DbScopes = _mainManager.SortUniqeIDsFromDbByScope(_mainManager.RetriveUniqeIDsFromDB());
 
-//               Console.WriteLine("verifying: " + Options.filePath.Trim());
-//               //verifying
-//               if (xmlScopes != null)
-//               {
-//                   // go through all the dictionaries and compare their values with db
-//                   if (_mainManager.CompareXmlScopesWithDBScopes(xmlScopes, DbScopes))
-//                   {
-//                       Console.WriteLine("no conflicts");
-
-//                       //if user selected the --update cliCommand option
-//                       if (Options.isUpdate)
-//                       {
-//                           _mainManager.UpdateDatabaseWithNewUniqueIds();
-//                       }
-//                   }
-//               }
-//               else
-//               {
-//                   _log.LogError("Can't read from xml file", LogProviderType.Console);
-//                   throw new Exception();
-//               }
-//           }
-//       })
-//       //if user enterd wrong arguments
-//       .WithNotParsed((errs) => throw new ArgumentException($"Failed to parse command line arguments: {errs}"));
-//    }
-//}
