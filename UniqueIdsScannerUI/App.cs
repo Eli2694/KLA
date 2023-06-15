3e2f28bf2c27bfc171279eea1f7c3a9984dc017a
Eli2694 <gilgureli@gmail.com>
2023-06-15 13:44:31 +0300

    update app.cs

--- UniqueIdsScannerUI/App.cs
+++ UniqueIdsScannerUI/App.cs
@@ -40,8 +40,10 @@ public class App
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Instructions:");
-            Console.WriteLine("For Updating The Database Use: UniqueIdsScanner.exe --update");
+            Console.WriteLine("Xml Paths Reside in Configuration file:");
             Console.WriteLine("For Verifying The Content Of Xml Files Use: UniqueIdsScanner.exe --verify");
+            Console.WriteLine("For Updating The Database Use : UniqueIdsScanner.exe --update");
+            Console.WriteLine("Input Xml Path:");
             Console.ResetColor();
             
         }
@@ -49,55 +51,106 @@ public class App
         {
 
             var listOfXmlFileFromAppSettings = _settings.GetSection("XmlFilesPath").Get<List<string>>();
-          
-            if(_mainManager.ValidateXmlFilePaths(listOfXmlFileFromAppSettings))
+
+            using (var parser = new CommandLine.Parser((settings) =>
             {
-                using (var parser = new CommandLine.Parser((settings) =>
-                {
-                    settings.CaseSensitive = true;
-                }))
-                {
-                    //add logs
-                    var result = Parser.Default.ParseArguments<CliOptions>(args)
-                   .WithParsed(Options => {
-                       if (string.IsNullOrWhiteSpace(Options.filePath))
-                       {
-                           Console.WriteLine("Invalid command line arguments");
-                           throw new ArgumentException("filePath is null or empty or white-space");
-                       }
-                       else
-                       {
-                           M_SeperatedScopes? xmlScopes = _mainManager.XmlToSeperatedScopes(Options.filePath);
-                           M_SeperatedScopes? DbScopes = _mainManager.SortUniqeIDsFromDbByScope(_mainManager.RetriveUniqeIDsFromDB());
+                settings.CaseSensitive = true;
+            }))
+            {
+                //add logs
+                var result = Parser.Default.ParseArguments<CliOptions>(args)
+               .WithParsed(Options => {
+                   if (string.IsNullOrWhiteSpace(Options.filePath))
+                   {
+                       Console.WriteLine("Invalid command line arguments");
+                       throw new ArgumentException("filePath is null or empty or white-space");
+                   }
+                   else
+                   {
 
-                           Console.WriteLine("verifying: " + Options.filePath.Trim());
-                           //verifying
-                           if (xmlScopes != null)
-                           {
-                               // go through all the dictionaries and compare their values with db
-                               if (_mainManager.CompareXmlScopesWithDBScopes(xmlScopes, DbScopes))
-                               {
-                                   Console.WriteLine("no conflicts");
+                       RunVerifyAndUpdate(Options.filePath, Options.isUpdate);
+                   }
+               })
+               //if user enterd wrong arguments
+               .WithNotParsed((errs) => throw new ArgumentException($"Failed to parse command line arguments: {errs}"));
+            }
+        }
+    }
 
-                                   //if user selected the --update cliCommand option
-                                   if (Options.isUpdate)
-                                   {
-                                       _mainManager.UpdateDatabaseWithNewUniqueIds();
-                                   }
-                               }
-                           }
-                           else
-                           {
-                               _log.LogError("Can't read from xml file", LogProviderType.Console);
-                               throw new Exception();
-                           }
-                       }
-                   })
-                   //if user enterd wrong arguments
-                   .WithNotParsed((errs) => throw new ArgumentException($"Failed to parse command line arguments: {errs}"));
+
+    public void RunVerifyAndUpdate(string filepath, bool isUpdate)
+    {
+        M_SeperatedScopes? xmlScopes = _mainManager.XmlToSeperatedScopes(filepath);
+        M_SeperatedScopes? DbScopes = _mainManager.SortUniqeIDsFromDbByScope(_mainManager.RetriveUniqeIDsFromDB());
+
+        Console.WriteLine("verifying: " + filepath.Trim());
+        //verifying
+        if (xmlScopes != null)
+        {
+            // go through all the dictionaries and compare their values with db
+            if (_mainManager.CompareXmlScopesWithDBScopes(xmlScopes, DbScopes))
+            {
+                Console.WriteLine("no conflicts");
+
+                //if user selected the --update cliCommand option
+                if (isUpdate)
+                {
+                    _mainManager.UpdateDatabaseWithNewUniqueIds();
                 }
             }
+        }
+        else
+        {
+            _log.LogError("Can't seperate xml file to scopes", LogProviderType.Console);
         }
     }
 }
 
+
+//if (_mainManager.ValidateXmlFilePaths(listOfXmlFileFromAppSettings))
+//{
+//    using (var parser = new CommandLine.Parser((settings) =>
+//    {
+//        settings.CaseSensitive = true;
+//    }))
+//    {
+//        //add logs
+//        var result = Parser.Default.ParseArguments<CliOptions>(args)
+//       .WithParsed(Options => {
+//           if (string.IsNullOrWhiteSpace(Options.filePath))
+//           {
+//               Console.WriteLine("Invalid command line arguments");
+//               throw new ArgumentException("filePath is null or empty or white-space");
+//           }
+//           else
+//           {
+//               M_SeperatedScopes? xmlScopes = _mainManager.XmlToSeperatedScopes(Options.filePath);
+//               M_SeperatedScopes? DbScopes = _mainManager.SortUniqeIDsFromDbByScope(_mainManager.RetriveUniqeIDsFromDB());
+
+//               Console.WriteLine("verifying: " + Options.filePath.Trim());
+//               //verifying
+//               if (xmlScopes != null)
+//               {
+//                   // go through all the dictionaries and compare their values with db
+//                   if (_mainManager.CompareXmlScopesWithDBScopes(xmlScopes, DbScopes))
+//                   {
+//                       Console.WriteLine("no conflicts");
+
+//                       //if user selected the --update cliCommand option
+//                       if (Options.isUpdate)
+//                       {
+//                           _mainManager.UpdateDatabaseWithNewUniqueIds();
+//                       }
+//                   }
+//               }
+//               else
+//               {
+//                   _log.LogError("Can't read from xml file", LogProviderType.Console);
+//                   throw new Exception();
+//               }
+//           }
+//       })
+//       //if user enterd wrong arguments
+//       .WithNotParsed((errs) => throw new ArgumentException($"Failed to parse command line arguments: {errs}"));
+//    }
+//}
